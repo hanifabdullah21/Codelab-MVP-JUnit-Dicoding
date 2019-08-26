8be747bc6321cd5508a2faad1bbd93be62e4f990
singpaulee <hanifabdullah7@gmail.com>
2019-08-26 16:53:50 +0700

    Create View

---
+++ app/src/main/java/com/singpaulee/codelab_mvp_junit_dicoding/view/MainView.kt
@@ -0,0 +1,7 @@
+package com.singpaulee.codelab_mvp_junit_dicoding.view
+
+import com.singpaulee.codelab_mvp_junit_dicoding.model.MainModel
+
+interface MainView {
+    fun showVolume(model: MainModel)
+}
